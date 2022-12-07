9f474f1f4df8af25c2fc2cef418c29795ebbf91c
tomifuchi <tomifuchi@yahoo.com>
2022-12-07 15:09:36 +0700

    Polishing state

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ function Book(title, author, year, genre, read) {
     this.id = Book.index++;//Simple but this will do for now
 }
 
+//Using index like this is fine, but to a certain point, because when we remove items,
+//it will not fill the empty index but kept counting forward. So, this is not optimal.
+//Ideally we want the index to be unique. And simple to maintain.
 Book.index = 0;
 
 //Create a new book push to myLibrary and associate it with the DOM element
@@ -21,15 +24,16 @@ function addBookToLibrary(title, author, year, genre, read) {
     function pushToBookShelf(book) {
         const book_shelf = document.getElementById('book-shelf');
             /*
-            a
-              li.book
-                .book-cover-container
-                  img(src="./img/bookcover_default.png", alt="", srcset="")#cover
-                h2.title Title goes here
-                h3.author Author goes here
-                p.year year goes here
-                button.remove Remove item
-                button.read Read
+              li.book.not-read //Default is not read class is applied
+                a
+                  .book-cover-container
+                    img(src="./img/bookcover_default.png", alt="", srcset="")#cover
+                  h2.title Title goes here
+                  h3.author Author goes here
+                  p.year year goes here
+                .book-operation-container
+                  button.remove Remove item
+                  button.read Read
             */
             const list_item_link = document.createElement('a');
             list_item_link.setAttribute('href','#');
@@ -38,8 +42,7 @@ function addBookToLibrary(title, author, year, genre, read) {
             const list_item = document.createElement('li');
             list_item.classList.add('book');
 
-
-            list_item_link.appendChild(list_item);
+            list_item.appendChild(list_item_link);
             //child 
             const book_cover_container = document.createElement('div');
             book_cover_container.classList.add('book-cover-container');
@@ -50,49 +53,62 @@ function addBookToLibrary(title, author, year, genre, read) {
 
             book_cover_container.appendChild(book_cover);
 
-            list_item.appendChild(book_cover_container);
+            list_item_link.appendChild(book_cover_container);
             //child
             const title = document.createElement('h2');
             title.classList.add('title');
             title.textContent = book.title;
 
-            list_item.appendChild(title);
+            list_item_link.appendChild(title);
             //child
             const author = document.createElement('h3');
             author.classList.add('author');
             author.textContent = book.author;
 
-            list_item.appendChild(author);
+            list_item_link.appendChild(author);
             //child
             const year = document.createElement('p');
             year.classList.add('year');
             year.textContent = book.year;
-            list_item.appendChild(year);
+            list_item_link.appendChild(year);
+
 
             //child
+            const book_operation_container = document.createElement('div');
+            book_operation_container.classList.add('book-operation-container');
+
+            //subchild
             const remove_button = document.createElement('button');
             //This might not be a good way to do it, but this is ok I guess
-            remove_button.innerHTML= 'Remove item' + '<i class="fa-solid fa-trash-can"></i>';
+            remove_button.innerHTML= 'Remove item ' + '<i class="fa-solid fa-trash-can"></i>';
             remove_button.addEventListener('click', () => {
                 //As simple as it gets
                 myLibrary.splice(myLibrary.indexOf(myLibrary.filter(item => item.id == book.id)[0]), 1);
-                book_shelf.removeChild(list_item_link);
+                book_shelf.removeChild(list_item);
             });
             remove_button.classList.add('remove');
-            list_item.appendChild(remove_button);
+            book_operation_container.appendChild(remove_button);
 
-            //child
+            //subchild
             const read_button = document.createElement('button');
             //This too, might not be a good way to do it, but this is ok I guess
-            read_button.innerHTML = 'Read' + '<i class="fa-solid fa-book"></i>';
+            read_button.innerHTML = 'Read ' + '<i class="fa-solid fa-book"></i>';
             read_button.addEventListener('click', () => {
                 book.read = (book.read == true) ? false:true; 
+                list_item.classList.toggle('did-read');
             });
             read_button.classList.add('read');
-            list_item.appendChild(read_button);
+            book_operation_container.appendChild(read_button);
 
+            list_item.appendChild(book_operation_container);
+
+            //Read check mark if read
+            if(book.read){
+                list_item.classList.toggle('did-read');
+            }
+            
             //Push to bookshelf
-            book_shelf.appendChild(list_item_link);
+            book_shelf.appendChild(list_item);
     }
     pushToBookShelf(new_book);
 }
@@ -147,6 +163,7 @@ book_add_btn.addEventListener('click', () => {
             Just an idea that's all.
         */
         if(!isDuplicate('')){
+            book_add_form.reset(); //Reset the form after submit
             addBookToLibrary(title, author, year, genre, wasRead);
         }
     }
@@ -159,3 +176,26 @@ function isDuplicate(author_title_id) {
     //This will always return false
     return false;
 }
+
+function randomDate() {
+    function randInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+    return `${randInt(1900, 1999)}-${randInt(1,12)}-${randInt(1,31)}`;
+}
+
+//Sample items
+addBookToLibrary('Catcher in the Rye', 'JD Catlinger', randomDate(), 'drama', true);
+addBookToLibrary('Thus spoke Zarathustra', 'Friedrich Neitzsche', randomDate(),'Philosophy', false);
+addBookToLibrary('Intelligent Investor', 'Warrent Buffett', randomDate(), 'Finance', true);
+addBookToLibrary('Something', 'Someone', randomDate(), 'Horror', false);
+addBookToLibrary('Another', 'Someone', randomDate(), 'Scifi', false);
+addBookToLibrary('Catcher in the Rye', 'JD Catlinger', randomDate(), 'drama', true);
+addBookToLibrary('Thus spoke Zarathustra', 'Friedrich Neitzsche', randomDate(),'Philosophy', false);
+addBookToLibrary('Catcher in the Rye', 'JD Catlinger', randomDate(), 'drama', true);
+addBookToLibrary('Thus spoke Zarathustra', 'Friedrich Neitzsche', randomDate(),'Philosophy', false);
+addBookToLibrary('Intelligent Investor', 'Warrent Buffett', randomDate(), 'Finance', true);
+addBookToLibrary('Something', 'Someone', randomDate(), 'Horror', false);
+addBookToLibrary('Another', 'Someone', randomDate(), 'Scifi', false);
+addBookToLibrary('Catcher in the Rye', 'JD Catlinger', randomDate(), 'drama', true);
+addBookToLibrary('Thus spoke Zarathustra', 'Friedrich Neitzsche', randomDate(),'Philosophy', false);
